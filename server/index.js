99871b58ec89c95b3a6a26b966fef543443631e9
apoorvarajlondhe <apoorvarajlondhe15@gmail.com>
2023-08-05 00:11:53 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+// Import and configure dotenv
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 import postRoutes from './routes/posts.js';
 
 const app = express();
@@ -14,9 +19,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 //23%40mongodb
-
-const CONNECTION_URL = 'mongodb+srv://crislearns1:appmongo123@cluster0.dijlwco.mongodb.net/student?retryWrites=true&w=majority';
+//mongodb+srv://crislearns1:appmongo123@cluster0.dijlwco.mongodb.net/student?retryWrites=true&w=majority
+const CONNECTION_URL = "mongodb+srv://crislearns1:appmongo123@cluster0.dijlwco.mongodb.net/student?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
+
 
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser:true,
