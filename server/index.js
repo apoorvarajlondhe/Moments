3f06d71f6247029fae0cddea82838a6c671a1e01
apoorvarajlondhe <apoorvarajlondhe15@gmail.com>
2023-08-05 02:16:50 +0530

    fourth commit

--- server/index.js
+++ server/index.js
@@ -12,12 +12,16 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+
+app.use('/posts', postRoutes);
+
+
 //23%40mongodb
 //mongodb+srv://crislearns1:appmongo123@cluster0.dijlwco.mongodb.net/student?retryWrites=true&w=majority
 const CONNECTION_URL = "mongodb+srv://crislearns1:appmongo123@cluster0.dijlwco.mongodb.net/student?retryWrites=true&w=majority";
